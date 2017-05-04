b4647439d49067c4fc8da226739e92ee4b58b72a
Cloud <kevia@triangle.host>
2017-05-04 16:35:51 -0700

    Added hotloading

--- app/index.js
+++ app/index.js
@@ -8,6 +8,7 @@ class App extends React.Component {
 
     return (
       <div>
+        Webpack HotLoading
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,45 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 
-  entry: './app/index.js',
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './app/index.js'
+  ],
 
   output: {
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
     filename: 'index_bundle.js'
   },
 
   module: {
     rules: [
-      {test: /\.(js|jsx)$/, use: 'babel-loader'},
+      {test: /\.(js|jsx)$/, exclude: /node_modules/,
+         use: {
+           loader: 'babel-loader',
+           options: {
+              presets: ['env', 'react']
+            }
+          }
+        },
       {test: /\.css$/, use: ['style-loader', 'css-loader']}
     ]
-  }
+  },
+
+  devServer: {
+    hot: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './app/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 }
