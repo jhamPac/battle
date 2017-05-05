33edb26521cbc28175fd8d434c41ca6d22e92758
Cloud <kevia@triangle.host>
2017-05-05 07:16:42 -0700

    Use subsitution for bundle names

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'index_bundle.js'
+    filename: '[name].bundle.js'
   },
 
   module: {
