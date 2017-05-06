345602d654a217ee4a9b0308f35b9b8a4e987529
Cloud <kevia@triangle.host>
2017-05-05 19:55:03 -0700

    Popular component done

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,12 +2,41 @@ import React from 'react';
 
 class Popular extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: 'All'
+    };
+
+    // always call in correct context; this component; only for eventHandlers
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(event) {
+    this.setState({ selectedLanguage: event.currentTarget.dataset.lang });
+  }
+
   render() {
 
+    let languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
     return (
-      <div>
-        <h1>Yeah</h1>
-      </div>
+      <ul className="languageUL">
+
+      {languages.map((language) => {
+
+        return (<li
+                  data-lang={language}
+                  key={language}
+                  onClick={this.updateLanguage}
+                  style={language === this.state.selectedLanguage ? { color: '#D0021b' } : null }>
+                  {language}
+              </li>);
+
+      })}
+
+      </ul>
     );
   }
 }
