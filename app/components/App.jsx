eeb892817089144684bda59afbeb567e3a35bc3b
Cloud <kevia@triangle.host>
2017-05-20 11:03:34 -0700

    Created test results component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,6 +3,7 @@ import { Home } from './Home';
 import { Nav }  from './Nav';
 import Popular  from './Popular';
 import Battle   from './Battle';
+import Results  from './Results';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 export default class App extends React.Component {
@@ -18,6 +19,7 @@ export default class App extends React.Component {
           <Switch>
             <Route exact path='/' component={ Home } />
             <Route exact path='/battle' component={ Battle } />
+            <Route exact path='/battle/results' component={ Results } />
             <Route path='/popular' component={ Popular } />
             <Route render={function() {
                 return <p>404 Not Found</p>
---
+++ app/components/Results.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Results extends React.Component {
+
+  render() {
+
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+
+  }
+
+};
