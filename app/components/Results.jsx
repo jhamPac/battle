65808164d8aed193590e81c3959eba8805b70128
Cloud <kevia@triangle.host>
2017-05-29 12:41:45 -0700

    Fleshing out Results component

--- app/components/Results.jsx
+++ app/components/Results.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import qs from 'query-string';
+import { battle } from '../utils/api';
 
 export default class Results extends React.Component {
+
+  componentDidMount = () => {
+    let players = qs.parse(this.props.location.search);
+
+    battle([
+      players.playerOneName,
+      players.playerTwoName
+    ]).then(function(results) {
+      console.log(results);
+    });
+  }
 
   render() {
 
