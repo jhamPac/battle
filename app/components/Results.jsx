f9be95a760c018f1a9d15d0b7d54a050e289865e
Cloud <kevia@triangle.host>
2017-06-03 10:59:27 -0700

    Fat arrows all the way

--- app/components/Results.jsx
+++ app/components/Results.jsx
@@ -4,14 +4,40 @@ import { battle } from '../utils/api';
 
 export default class Results extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true
+    }
+  }
+
   componentDidMount = () => {
+
     let players = qs.parse(this.props.location.search);
 
     battle([
       players.playerOneName,
       players.playerTwoName
-    ]).then(function(results) {
-      console.log(results);
+    ]).then((players) => {
+
+      if (players === null) {
+        return this.setState({
+          error: 'Error: Make sure both players exist',
+          loading: false
+        });
+      }
+
+      this.setState({
+        winner: players[0],
+        loser: players[1],
+        error: null,
+        loading: false
+      });
+
     });
   }
 
